d88f6678e934c0cc663c4074a9ab51efd314a1ba
Apoorva-lokhande <apoorva.lokhande1@gmail.com>
2020-06-02 10:33:17 +0530

    gamersback123

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,10 @@ const App = () => {
             <BookingHistory />
           </Route>
 
+          <Route  path="/">
+            <Login />
+          </Route>
+
           <Route  path="/profile">
             <UserDetails />
           </Route>
